8d060ba7a609fabd1306b84b23ba0be6f2b8c016
wuhaoyuan <wuhaoyuan@supermap.com>
2020-04-30 11:18:44 +0800

    css书写方式 使用Fragment替代外层div包裹

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import TodoItem from "./TodoItem"
 
-class TodoList extends React.Component {
+class TodoList extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -38,11 +38,11 @@ class TodoList extends React.Component {
   }
   render() {
     return (
-      <div>
+      <Fragment>
         <input value={this.state.inputVal} onChange={this.handleInputChange} />
-        <button onClick={this.handleBtnClick}>add</button>
+        <button className='red-btn' onClick={this.handleBtnClick}>add</button>
         <ul>{this.getTodoItem()}</ul>
-      </div>
+      </Fragment>
     )
   }
 }
